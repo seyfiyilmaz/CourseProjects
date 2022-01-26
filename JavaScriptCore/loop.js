af0ec1ef2aa465c3794d1ec26b96ff502291c4f0
Seyfi YILMAZ <seyfi.yilmaz.ca@gmail.com>
2022-01-26 11:00:48 +0300

    preclass for loop(homework)

--- JavaScriptCore/loop.js
+++ JavaScriptCore/loop.js
@@ -29,14 +29,78 @@ while(i<0)
 */
 
 //foreach ---ES5 ile geldi---
-
-/*myArray.forEach(function(item, index){
+/*
+myArray.forEach(function(item, index){
+    // if(i === 1) break; //Bu kod hata verir forEach içinde break kullanılmaz.
     console.log(`index: ${index}, value: ${item}`);
-});*/
+});
 //myArray.forEach((item, index) => console.log(`index: ${index}, value: ${item}`));
-myArray.forEach(item=> console.log(`value: ${item}`));
+//myArray.forEach(item=> console.log(`value: ${item}`));
+*/
 
+// for...of  --ES6 ile geldi
+/*for (let elem of myArray) {
+    console.log(`value: , ${elem}`);
+}*/
 
-// for...of
+/*for(const elem  of myArray) {
+    console.log(`value: , ${elem}`);
+}*/
+
+/*for(const [index, elem]  of myArray.entries()) {
+    // if(index === 1) break;
+    **if(index === 1) continue;
+    console.log(`index: ${index}, value: , ${elem}`);
+}*/
+
 
 //for...in
+/*const myObject = {
+    "a": 1,
+    "b": 2,
+    "c": 3
+};
+
+for(let property in myObject){
+    console.log(`property: ${property}, value: , ${myObject[property]}`);
+}*/
+
+const num = 100000000;
+const arr = new Array(num);
+
+console.time("for");
+for (let i = 0; i<num;  i++){}
+console.timeEnd("for");
+
+console.time("while");
+let i = 0;
+while (i < 0){i++;}
+console.timeEnd("while");
+
+console.time("forEach");
+arr.forEach(elem => "");
+console.timeEnd("forEach");
+
+console.time("for...of");
+for(const elem of arr) {}
+console.timeEnd("for...of");
+/*for 1.000.000 iteration
+    for: 2.587ms
+    while: 0.004ms
+    forEach: 2.889ms
+    for...of: 19.112ms
+*/
+
+/*for 10.000.000 iteration
+    for: 7.108ms
+    while: 0.004ms
+    forEach: 29.336ms
+    for...of: 157.366ms
+*/
+
+/*for 100.000.000 iteration
+    for: 52.429ms
+    while: 0.006ms
+    forEach: 2.453s
+    for...of: 2.676s
+*/
