73d7e90379cf140ba092a291525c378452fd8a7d
Seyfi YILMAZ <seyfi.yilmaz.ca@gmail.com>
2022-01-31 10:27:15 +0300

    workpush1

--- JavaScriptCore/calculators.js
+++ JavaScriptCore/calculators.js
@@ -1,52 +1,55 @@
-let process_mark = "";
-let processresult = 0;
+function calculate() {
+  let process_mark = "";
+  let processresult = 0;
 
-while (true) {
-  let a, b = 0;
-  a = prompt("Enter the first number for four operations in math.", "0");
-  process_mark = prompt("Please, you must select ['+','-','*','/'] for four operations in math. ", "+"); 
-  b = prompt("Enter the second number for four operations in math.", "0");
-  alert("A value: " + a);
-  alert("B value: " + b);
-  if(a == "q" || b == "q")
-    break;
-  else{
-    a = parseInt(a);
-    b = parseInt(b);
-  }
+  while (true) {
+    let a,
+      b = 0;
+    a = prompt("Enter the first number for four operations in math.", "0");
+    process_mark = prompt(
+      "Please, you must select ['+','-','*','/'] for four operations in math. ",
+      "+"
+    );
+    b = prompt("Enter the second number for four operations in math.", "0");
 
-  if(!checknumbers(a, b)){
-    console.log("Please, You check your input values that these must be number value!");
-    alert("Mistake input value...");
-    continue;
-  }
-  //https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt
-  switch (process_mark) {
-    case "+":
-      processresult = a + b;
-      break;
-    case "-":
-      processresult = a - b;
-      break;
-    case "*":
-      processresult = a * b;
-      break;
-    case "/":
-      processresult = a / b;
-      break;
-    default:
+    if (a == "q" || b == "q") break;
+    else {
+      a = parseInt(a);
+      b = parseInt(b);
+    }
+    if (!checknumbers(a, b)) {
       console.log(
-        "Please, you must select ['+','-','*','/'] for four operations in math. "
+        "Please, You check your input values that these must be number value!"
       );
-      break;
+      alert("Mistake input value. \nPlease, You check your input values that these must be number value!");
+      continue;
+    }
+
+    switch (process_mark) {
+      case "+":
+        processresult = a + b;
+        break;
+      case "-":
+        processresult = a - b;
+        break;
+      case "*":
+        processresult = a * b;
+        break;
+      case "/":
+        processresult = a / b;
+        break;
+      default:
+        // console.log(
+        //   "Please, you must select ['+','-','*','/'] for four operations in math. "
+        // );
+        alert("Please, you must select ['+','-','*','/'] for four operations in math. ");
+        break;
+    }
+    console.log("Operation result: ", processresult);
   }
-  console.log("Operation result: ", processresult);
 }
 function checknumbers(a, b) {
-  a = parseInt(a);
-  b = parseInt(b);
-  console.log("typeof a: ", typeof a);
-  console.log("typeof b: ", typeof b);
-  if (typeof a === "number" && typeof b === "number") return true;
+  if (isNaN(parseInt(a)) || isNaN(parseInt(b))) return false;
+  else if (typeof a === "number" && typeof b === "number") return true;
   else return false;
 }
